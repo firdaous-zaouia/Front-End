8b38dc67a09af826a6b1905274d9faa943bfe5fb
Zaouia Firdaous <zaouia.firdaous@gmail.com>
2024-07-01 17:35:22 +0200

    TP3 Javascript

---
+++ JavaScript/TP3/script.js
@@ -0,0 +1,36 @@
+// Créer un événement au scroll
+
+// Réduire la navbar quand on descend vers le vite, la remettre à sa taille initiale si on remonte tout en haut
+
+// Faire apparaitre l'image de la partie improvise
+
+// Faire apparaitre la popup quand on est en bas du site
+
+// Bonus : quand on clicke sur la popup elle disparait pour toujours
+let playOne = true;
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 50) {
+    navbar.style.height = "45px";
+  } else {
+    navbar.style.height = "90px";
+  }
+  let scrollValue =
+    (window.scrollY + window.innerHeight) / document.body.offsetHeight;
+
+  if (scrollValue > 0.45) {
+    imgImprovise.style.opacity = 1;
+    imgImprovise.style.transform = "none";
+  }
+  //Popup
+  if (scrollValue > 0.85 && playOne) {
+    popup.style.opacity = 1;
+    popup.style.transform = "none";
+    playOne = false;
+  }
+});
+
+closeBtn.addEventListener("click", () => {
+  popup.style.opacity = 0;
+  popup.style.transform = "translate(500px)";
+});
