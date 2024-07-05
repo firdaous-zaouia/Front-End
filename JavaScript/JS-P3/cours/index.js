2146bc4ececfc52f248a400ac6d4ed3abd0ef4ab
Zaouia Firdaous <zaouia.firdaous@gmail.com>
2024-07-05 06:44:44 +0200

    Cours et TextAnime

---
+++ JavaScript/JS-P3/cours/index.js
@@ -0,0 +1,10 @@
+//Rappel des types de donees
+let string = "chaine";
+let number = 25;
+let boolean = true;
+let maVariable; //Undefined
+
+//Tableaux
+let array = ["Bordeau", "Toulouse", "Nantes"];
+//console.log(array[0][3]);
+let array = ["bordeau", 24, true, [1, 2], { nom: "denis" }];
---
+++ JavaScript/JS-P3/text-anim/index.js
@@ -0,0 +1,31 @@
+const target = document.getElementById("target");
+let array = ["développeur", "photographe", "créatif"];
+let wordIndex = 0;
+let letterIndex = 0;
+
+const createLetter = () => {
+  const letter = document.createElement("span");
+  target.appendChild(letter);
+
+  letter.textContent = array[wordIndex][letterIndex];
+  setTimeout(() => {
+    letter.remove();
+  }, 2000);
+};
+
+const loop = () => {
+  setTimeout(() => {
+    if (letterIndex < array[wordIndex].length) {
+      createLetter();
+      letterIndex++;
+      loop();
+    } else {
+      wordIndex++;
+      letterIndex = 0;
+      setTimeout(() => {
+        loop();
+      }, 2800);
+    }
+  }, 60);
+};
+loop();
